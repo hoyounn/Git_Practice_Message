b51fd95acb27fa1bd48e3128b41f15cc84eab591
Unknown <0219alice@gmail.com>
2018-06-14 11:52:09 +0900

    DTO완성
    완성

--- src/kh/web/message/MessageDTO.java
+++ src/kh/web/message/MessageDTO.java
@@ -1,5 +1,38 @@
 package kh.web.message;
 
 public class MessageDTO {
-
+	private int seq;
+	private String name;
+	private String message;
+	
+	public MessageDTO() {
+		super();
+	}
+	
+	public MessageDTO(int seq, String name, String message) {
+		super();
+		this.seq = seq;
+		this.name = name;
+		this.message = message;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
 }
