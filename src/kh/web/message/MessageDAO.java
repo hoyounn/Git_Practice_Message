6ba20d782ee999d4ac484d0db60179779aa899af
Unknown <0219alice@gmail.com>
2018-06-14 12:05:28 +0900

    DAO 완성
    DAO

--- src/kh/web/message/MessageDAO.java
+++ src/kh/web/message/MessageDAO.java
@@ -1,5 +1,65 @@
 package kh.web.message;
 
-public class MessageDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MessageDAO {
+	private Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String id = "kh";
+		String pw = "kh";
+
+		Connection con = DriverManager.getConnection(url, id, pw);
+
+		return con;
+	}
+	
+	public int insertData(String name, String msg) throws Exception{
+		Connection con = this.getConnection();
+		String sql = "insert into messages values((message_seq.nextval,?,?)";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		
+		pstat.setString(1, name);
+		pstat.setString(2, msg);
+		
+		int result = pstat.executeUpdate();
+		
+		con.commit();
+		con.close();
+		pstat.close();
+		
+		return result;
+	}
+	
+	public List<MessageDTO> viewAllData() throws Exception{
+		Connection con = this.getConnection();
+		String sql = "select * from messages";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		
+		ResultSet rs = pstat.executeQuery();
+		List<MessageDTO> result = new ArrayList<>();
+		
+		while(rs.next()) {
+			MessageDTO dto = new MessageDTO();
+			
+			dto.setSeq(rs.getInt(1));
+			dto.setName(rs.getString(2));
+			dto.setMessage(rs.getString(3));
+			
+			result.add(dto);
+		}
+		
+		con.close();
+		pstat.close();
+		rs.close();
+		
+		return result;
+	}
+	
 }
